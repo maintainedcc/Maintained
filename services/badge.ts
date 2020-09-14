5606f3b03836cdc077950a1cb29fa763eab0e32a
Kyle <sdbagel@gmail.com>
2020-09-14 00:22:28 -0700

    Icon service using simpleicons

--- services/badge.ts
+++ services/badge.ts
@@ -1,14 +1,21 @@
 
 import { Badge, BadgeStyle, BadgeColor } from './data.ts';
+import { IconService } from "./icon.ts";
 
 interface MappedBadge extends Badge {
   keyCString?: string,
   valCString?: string,
+  keyIconURI?: string,
+  valIconURI?: string,
   dvsValue?: string
 }
 
 export class BadgeService {
-  constructor() {}
+  private iconService: IconService;
+
+  constructor() {
+    this.iconService = new IconService();
+  }
 
   // Returns an SVG string
   async badge(badge: Badge): Promise<string> {
@@ -32,6 +39,9 @@ export class BadgeService {
     m.valCString = this.colorMap(badge.valueColor);
 
     m.dvsValue = badge.valueSource ? await this.dvsFetch(badge.valueSource ?? "") : undefined;
+
+    //m.keyIconURI = await this.iconService.getIconDataURL("microsoftonenote", true);
+    //m.valIconURI = await this.iconService.getIconDataURL("microsoftword", false);
 
     return m;
   }
@@ -63,8 +73,21 @@ export class BadgeService {
 
   async plastic(badge: MappedBadge): Promise<string> {
     // Map badge widths
-    const keyW = badge.titleWidth;
+    let keyW = badge.titleWidth;
     let valW = badge.valueWidth;
+
+    // If an icon exists, shift the widths
+    let keyX = keyW / 2;
+    if (badge.keyIconURI) {
+      keyW += 30;
+      keyX += 25;
+    }
+    let valX = keyW + (valW / 2);
+    let valWO = 0;
+    if (badge.valIconURI) {
+      valWO = 30;
+      valX += 25;
+    }
 
     // Plastic style gradient
     const gradientDef = `
@@ -88,16 +111,18 @@ export class BadgeService {
     else {
       valW = badge.dvsValue ? badge.dvsValue.length * 5.2 + 30 : badge.valueWidth;
       return `
-      <svg xmlns="http://www.w3.org/2000/svg" width="${keyW + valW}" height="20">
+      <svg xmlns="http://www.w3.org/2000/svg" width="${keyW + valW + valWO}" height="20">
         ${gradientDef}
         <rect rx="3" width="${keyW + valW}" height="20" fill="${badge.keyCString}"/>
-        <rect rx="3" x="${keyW}" width="${valW}" height="20" fill="${badge.valCString}"/>
-        <rect rx="3" width="${keyW + valW}" height="20" fill="url(#a)"/>
+        <rect rx="3" x="${keyW}" width="${valW + valWO}" height="20" fill="${badge.valCString}"/>
+        <rect rx="3" width="${keyW + valW + valWO}" height="20" fill="url(#a)"/>
         <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-          <text x="${keyW / 2}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
-          <text x="${keyW / 2}" y="14">${badge.title}</text>
-          <text x="${keyW + (valW / 2)}" y="15" fill="#010101" fill-opacity=".3">${badge.dvsValue ?? badge.value}</text>
-          <text x="${keyW + (valW / 2)}" y="14">${badge.dvsValue ?? badge.value}</text>
+          <image x="12" y="3" height="14px" width="14px" href="${badge.keyIconURI ?? ""}" />
+          <text x="${keyX}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
+          <text x="${keyX}" y="14">${badge.title}</text>
+          <image x="${keyW + 12}" y="3" height="14px" width="14px" href="${badge.valIconURI ?? ""}" />
+          <text x="${valX}" y="15" fill="#010101" fill-opacity=".3">${badge.dvsValue ?? badge.value}</text>
+          <text x="${valX}" y="14">${badge.dvsValue ?? badge.value}</text>
         </g>
       </svg>`;
     }
@@ -105,29 +130,45 @@ export class BadgeService {
 
   async flat(badge: MappedBadge): Promise<string> {
     // Map badge widths
-    const keyW = badge.titleWidth;
+    let keyW = badge.titleWidth;
     let valW = badge.valueWidth;
+
+    // If an icon exists, shift the widths
+    let keyX = keyW / 2;
+    if (badge.keyIconURI) {
+      keyW += 30;
+      keyX += 25;
+    }
+    let valX = keyW + (valW / 2);
+    let valWO = 0;
+    if (badge.valIconURI) {
+      valWO = 30;
+      valX += 25;
+    }
 
     if (badge.isMono)
       return `
       <svg xmlns="http://www.w3.org/2000/svg" width="${keyW}" height="20">
         <rect width="${keyW}" height="20" fill="${badge.keyCString}"/>
         <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-          <text x="${keyW / 2}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
-          <text x="${keyW / 2}" y="14">${badge.title}</text>
+          <image x="12" y="3" height="14px" width="14px" href="${badge.keyIconURI ?? ""}" />
+          <text x="${keyX}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
+          <text x="${keyX}" y="14">${badge.title}</text>
         </g>
       </svg>`;
     else {
       valW = badge.dvsValue ? badge.dvsValue.length * 5.2 + 30 : badge.valueWidth;
       return `
-      <svg xmlns="http://www.w3.org/2000/svg" width="${keyW + valW}" height="20">
+      <svg xmlns="http://www.w3.org/2000/svg" width="${keyW + valW + valWO}" height="20">
         <rect width="${keyW + valW}" height="20" fill="${badge.keyCString}"/>
-        <rect x="${keyW}" width="${valW}" height="20" fill="${badge.valCString}"/>
+        <rect x="${keyW}" width="${valW + valWO}" height="20" fill="${badge.valCString}"/>
         <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-          <text x="${keyW / 2}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
-          <text x="${keyW / 2}" y="14">${badge.title}</text>
-          <text x="${keyW + (valW / 2)}" y="15" fill="#010101" fill-opacity=".3">${badge.dvsValue ?? badge.value}</text>
-          <text x="${keyW + (valW / 2)}" y="14">${badge.dvsValue ?? badge.value}</text>
+          <image x="12" y="3" height="14px" width="14px" href="${badge.keyIconURI ?? ""}" />
+          <text x="${keyX}" y="15" fill="#010101" fill-opacity=".3">${badge.title}</text>
+          <text x="${keyX}" y="14">${badge.title}</text>
+          <image x="${keyW + 12}" y="3" height="14px" width="14px" href="${badge.valIconURI ?? ""}" />
+          <text x="${valX}" y="15" fill="#010101" fill-opacity=".3">${badge.dvsValue ?? badge.value}</text>
+          <text x="${valX}" y="14">${badge.dvsValue ?? badge.value}</text>
         </g>
       </svg>`;
     }
@@ -135,29 +176,46 @@ export class BadgeService {
 
   async ftb(badge: MappedBadge): Promise<string> {
     // Map badge widths
-    const keyW = badge.titleWidth + 30;
+    let keyW = badge.titleWidth + 30;
     let valW = badge.valueWidth + 30;
 
+    // Text transform
     badge.title = badge.title.toUpperCase();
     badge.value = badge.value.toUpperCase();
+
+    // If an icon exists, shift the widths
+    let keyX = keyW / 2;
+    if (badge.keyIconURI) {
+      keyW += 34;
+      keyX += 28;
+    }
+    let valX = keyW + (valW / 2);
+    let valWO = 0;
+    if (badge.valIconURI) {
+      valWO = 34;
+      valX += 28;
+    }
 
     if (badge.isMono)
       return `
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${keyW}" height="28">
         <rect width="${keyW}" height="28" fill="${badge.keyCString}"/>
         <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="10">
-          <text x="${keyW / 2}" y="17" textLength="${badge.titleWidth}">${badge.title}</text>
+          <image x="12" y="3" height="20px" width="20px" href="${badge.keyIconURI ?? ""}" />
+          <text x="${keyX}" y="17" textLength="${badge.titleWidth}">${badge.title}</text>
         </g>
       </svg>`;
     else {
       valW = badge.dvsValue ? badge.dvsValue.length * 8 + 30 : valW;
       return `
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${keyW + valW}" height="28">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${keyW + valW + valWO}" height="28">
         <rect width="${keyW}" height="28" fill="${badge.keyCString}"/>
-        <rect x="${keyW}" width="${valW}" height="28" fill="${badge.valCString}"/>
+        <rect x="${keyW}" width="${valW + valWO}" height="28" fill="${badge.valCString}"/>
         <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="10">
-          <text x="${(keyW / 2)}" y="17" textLength="${badge.titleWidth}">${badge.title}</text>
-          <text x="${keyW + (valW / 2)}" y="17" font-weight="bold" textLength="${valW - 30}">${badge.dvsValue?.toUpperCase() ?? badge.value}</text>
+          <image x="12" y="3" height="20px" width="20px" href="${badge.keyIconURI ?? ""}" />
+          <text x="${keyX}" y="17" textLength="${badge.titleWidth}">${badge.title}</text>
+          <image x="${keyW + 12}" y="3" height="20px" width="20px" href="${badge.valIconURI ?? ""}" />
+          <text x="${valX}" y="17" font-weight="bold" textLength="${valW - 30}">${badge.dvsValue?.toUpperCase() ?? badge.value}</text>
         </g>
       </svg>`;
     }
---
+++ services/icon.ts
@@ -0,0 +1,23 @@
+
+import { exists } from "https://deno.land/std/fs/exists.ts";
+
+export class IconService {
+  constructor() {}
+
+  // Returns an SVG string or empty string
+  // Gets icons from simpleicons
+  async getIconDataURL(name: string, forceLight: boolean): Promise<string> {
+    const path = `services/simpleicons/icons/${name}.svg`;
+    if (await exists(path)) {
+      const decoder = new TextDecoder("utf-8");
+      let svgString = decoder.decode(await Deno.readFile(path));
+
+      // Force fill white
+      if (forceLight)
+        svgString = svgString.replace("<path", "<path fill='#fff'");
+
+      return "data:image/svg+xml;base64," + btoa(svgString);
+    }
+    else return "";
+  }
+}
