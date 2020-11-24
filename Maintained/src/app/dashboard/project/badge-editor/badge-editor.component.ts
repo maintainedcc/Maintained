2140640383bb5cc821ac0340476789814a1f69c0
Kyle <sdbagel@gmail.com>
2020-11-23 18:22:21 -0800

    Feed badge info to components

--- Maintained/src/app/dashboard/project/badge-editor/badge-editor.component.ts
+++ Maintained/src/app/dashboard/project/badge-editor/badge-editor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Badge } from 'src/app/core/schemas';
 
 @Component({
   selector: 'project-badge-editor',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./badge-editor.component.scss']
 })
 export class BadgeEditorComponent implements OnInit {
+  @Input() badge?: Badge;
 
   constructor() { }
 
