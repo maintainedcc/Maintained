d14919f0cb5ea3689c74efee6142d127f9470069
SDBagel <sdbagel@gmail.com>
2021-12-30 15:56:22 -0800

    Add fonts and temp example

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss" global>
+	main {
+		font-family: "Urbanist", sans-serif;
+		max-width: 1600px;
+		margin: auto;
+	}
+
+	h1, h2, h3, h4, h5, h6 {
+		font-family: "Poppins", sans-serif;
+		font-weight: 500;
+	}
+
+	code, pre {
+		background-color: #0001;
+		border-radius: 5px;
+		color: #555;
+		font-family: "Fira Mono", monospace;
+		font-size: 0.85em;
+		padding: 1px 6px;
+	}
+</style>
