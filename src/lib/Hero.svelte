edfbc1f1eb564766ee8e86fd10f5aa845b7c5411
SDBagel <sdbagel@gmail.com>
2022-02-02 15:08:04 -0800

    refactor: Hero component

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,74 @@
+
+<script lang="ts">
+	import BadgeButton from "$lib/BadgeButton.svelte";
+</script>
+
+<section class="hero">
+	<div class="left-deco a"></div>
+	<div class="left-deco b"></div>
+	<div class="left-deco c"></div>
+	<h1>maintained</h1>
+	<h2>Overengineered repository management</h2>
+	<BadgeButton href="http://localhost:8999/oauth/login"
+		left="Get Started" right="With GitHub" />
+</section>
+
+<style lang="scss">
+  .hero {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		justify-content: center;
+		min-height: 60vh;
+
+		.left-deco {
+			-webkit-clip-path: polygon(0 0, 100% 100%, 0% 100%);
+			clip-path: polygon(0 0, 100% 100%, 0% 100%);
+			position: absolute;
+			left: 0;
+
+			&.a {
+				background-color: #fa0;
+				height: 25vh;
+				width: 40vw;
+				top: 35vh;
+				z-index: 2;
+			}
+			&.b {
+				background-color: #fc5;
+				height: 35vh;
+				width: 55vw;
+				top: 25vh;
+				z-index: 1;
+			}
+			&.c {
+				background-color: #fd9;
+				height: 45vh;
+				width: 70vw;
+				top: 15vh;
+				z-index: 0;
+			}
+		}
+	}
+
+	h1 {
+		background-color: #fff;
+		background-clip: text;
+		color: transparent;
+		line-height: 1;
+		font-size: 7rem;
+		margin: 0;
+		margin-top: 5vh;
+
+		width: max-content;
+		position: relative;
+	}
+
+	h2 {
+		color: #f88;
+		font-size: 1.8rem;
+		font-weight: 700;
+		margin-top: 0;
+		text-transform: uppercase;
+	}
+</style>
