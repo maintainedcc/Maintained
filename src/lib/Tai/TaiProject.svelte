2f230c611ffc1f19e1c42b3afa4b7d5a0510cbc0
SDBagel <sdbagel@gmail.com>
2022-03-06 14:19:08 -0800

    feat: Add create badge bar button to Tai project

--- src/lib/Tai/TaiProject.svelte
+++ src/lib/Tai/TaiProject.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import BadgeEditor from "$lib/Tai/BadgeEditor.svelte";
+	import { createBadge } from "$lib/util/api";
 	import type { Project } from "$lib/util/schema";
 
 	export let project: Project;
@@ -9,6 +10,7 @@
 	{#each project.badges as badge}
 		<BadgeEditor bind:badge {project} />
 	{/each}
+	<button on:click={() => createBadge(project.title)}>Create New Badge</button>
 </div>
 
 <style lang="scss">
@@ -17,5 +19,28 @@
 		flex-direction: column;
 		row-gap: 10px;
 		flex: 1 1;
+	}
+
+	button {
+		background-color: transparent;
+		border: var(--blue) 1px solid;
+		border-radius: 3px;
+		box-sizing: border-box;
+		cursor: pointer;
+
+		font-family: inherit;
+		font-size: 0.6rem;
+		letter-spacing: 1px;
+		text-transform: uppercase;
+
+		margin: 0;
+		padding: 8px 15px;
+
+		transition-duration: 0.2s;
+
+		&:hover {
+			background-color: var(--blue);
+			color: white;
+		}
 	}
 </style>
